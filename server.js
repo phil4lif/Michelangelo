17c52443c48fe8e3a615faaa7d2178f56ef05b13
phil cowan <philipjcowan1@gmail.com>
2020-03-25 11:37:23 -0700

    writes user.js model and login member and signup jquery as well as makes a login sign up and members html adds user routes to html and api routes for users

--- server.js
+++ server.js
@@ -5,7 +5,9 @@
 // *** Dependencies
 // =============================================================
 var express = require("express");
-
+var session = require("express-session");
+// Requiring passport as we've configured it
+var passport = require("./config/passport");
 // Sets up the Express App
 // =============================================================
 var app = express();
@@ -20,16 +22,19 @@ app.use(express.json());
 
 // Static directory
 app.use(express.static("public"));
-
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 // Routes
 // =============================================================
 require("./routes/html-routes.js")(app);
 require("./routes/author-api-routes.js")(app);
 require("./routes/post-api-routes.js")(app);
+require("./routes/api-routes.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync({ force: true }).then(function() {
+db.sequelize.sync({ force: false }).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
